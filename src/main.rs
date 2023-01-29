5659f5eb0286f0e4e124ac3f81712af67eb0ec57
David Vernon <davidhvernon@mac.com>
2023-01-29 15:38:39 -0700

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use crate::tidii::scan_dir_for_old_files;
-use crate::utilities::get_log_fn;
 use dirs::desktop_dir;
 use std::thread::sleep;
 use std::time::Duration;
-use utilities::hours_to_sec;
+use tidii::scan_dir_for_old_files;
+use utilities::{get_log_fn, hours_to_sec};
 
 mod tidii;
 mod utilities;
 
 fn main() {
     let log_fn = get_log_fn();
-    let dir_path = desktop_dir().expect("Could not find the desktop directory.");
+    let desktop_dir_path = desktop_dir().expect("Could not find the desktop directory.");
 
     loop {
-        match scan_dir_for_old_files(&dir_path, 8, &log_fn) {
+        match scan_dir_for_old_files(&desktop_dir_path, 8, &log_fn) {
             Ok(()) => {
                 log_fn("Scan Complete");
                 sleep(Duration::from_secs(hours_to_sec(12)));
