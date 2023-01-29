55309b8f45e01415f0fde0e7d2b2ae46550b894d
David Vernon <davidhvernon@mac.com>
2023-01-29 15:33:43 -0700

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::time::SystemTime;
-
 use crate::tidii::scan_dir_for_old_files;
 use crate::utilities::get_log_fn;
 use dirs::desktop_dir;
+use std::thread::sleep;
 use std::time::Duration;
 use utilities::hours_to_sec;
 
@@ -13,19 +12,15 @@ fn main() {
     let log_fn = get_log_fn();
     let dir_path = desktop_dir().expect("Could not find the desktop directory.");
 
-    let mut next_time_to_run = SystemTime::now();
     loop {
-        let now = SystemTime::now();
-        if now > next_time_to_run {
-            match scan_dir_for_old_files(&dir_path, 8, &log_fn) {
-                Ok(()) => {
-                    log_fn("Scan Complete");
-                }
-                Err(err) => {
-                    log_fn(&format!("{:#?}", err));
-                }
+        match scan_dir_for_old_files(&dir_path, 8, &log_fn) {
+            Ok(()) => {
+                log_fn("Scan Complete");
+                sleep(Duration::from_secs(hours_to_sec(12)));
+            }
+            Err(err) => {
+                log_fn(&format!("{:#?}", err));
             }
         }
-        next_time_to_run = now + Duration::from_secs(hours_to_sec(12));
     }
 }
